0db3d024765d4ca466f4041c1bd7777e126d7628
minngz <minngz96@gmail.com>
2019-06-04 16:40:20 +0900

    [Add] top tab menu of Home

---
+++ app/src/main/java/com/example/manzbuy/HomeTap.java
@@ -0,0 +1,21 @@
+package com.example.manzbuy;
+
+import android.content.Context;
+
+import static android.view.View.inflate;
+
+public class HomeTap {
+    private Context context;
+
+    public HomeTap(Context context) {
+        //super(context);
+        init();
+    }
+
+
+    private void init() {
+        //this.context = getContext();
+        //inflate(getContext(), R.layout.home_tap, this);
+    }
+
+}
--- app/src/main/java/com/example/manzbuy/MainActivity.java
+++ app/src/main/java/com/example/manzbuy/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.manzbuy;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -32,12 +35,41 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private TabLayout homeTapLayout;
+    private ViewPager viewPager;
+    private HomePagerAdapter homePagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
         bottomNavView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        homeTapLayout = (TabLayout) findViewById(R.id.home_layout_tab);
+        homeTapLayout.addTab(homeTapLayout.newTab().setText("Home"));
+        homeTapLayout.addTab(homeTapLayout.newTab().setText("New"));
+        homeTapLayout.addTab(homeTapLayout.newTab().setText("Best"));
+        homeTapLayout.addTab(homeTapLayout.newTab().setText("Event"));
+
+        viewPager = (ViewPager)findViewById(R.id.home_pager_content);
+        homePagerAdapter = new HomePagerAdapter(getSupportFragmentManager(), homeTapLayout.getTabCount());
+        viewPager.setAdapter(homePagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(homeTapLayout));
+        homeTapLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(TabLayout.Tab tab){
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab){
+
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab){
+
+            }
+        });
     }
 
 }
