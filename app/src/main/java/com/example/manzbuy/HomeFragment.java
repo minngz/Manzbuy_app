1eb6ff59c350ceafe4d4ba4eaada4484567b0118
minngz <minngz96@gmail.com>
2019-06-07 18:12:54 +0900

    [Add] template of top tabs (home, new, best, event)

--- app/src/main/java/com/example/manzbuy/HomeFragment.java
+++ app/src/main/java/com/example/manzbuy/HomeFragment.java
@@ -1,6 +1,28 @@
 package com.example.manzbuy;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 public class HomeFragment extends Fragment {
+    public HomeFragment(){
+
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_home, container, false);
+        return view;
+    }
 }
